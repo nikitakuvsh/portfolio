6871f2a53c897c4f941cafb4a0b7c5d1270ca940
nikitakuvsh <world_fre@mail.ru>
2025-01-30 19:26:55 +0300

    fix styles

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -13,11 +13,9 @@ export default function Project() {
         return <div className="project__container">Проект не найден</div>;
     }
 
-    // Определяем, можно ли перейти на следующий / предыдущий проект
     const canClickNext = currentId < projectsData.length;
     const canClickPrev = currentId > 1;
 
-    // Функция перехода на следующий проект
     const nextProject = () => {
         if (canClickNext) {
             navigate(`/projects/${currentId + 1}`);
@@ -25,7 +23,6 @@ export default function Project() {
         }
     };
 
-    // Функция перехода на предыдущий проект
     const prevProject = () => {
         if (canClickPrev) {
             navigate(`/projects/${currentId - 1}`);
@@ -36,14 +33,11 @@ export default function Project() {
     return (
         <div className="project__container">
             <div className="project__content">
-                {/* Левая колонка */}
                 <div className="project__info">
                     <h1 className="project__title">{project.title}</h1>
                     <p className="project__description">{project.description}</p>
-                    <button className="project__button project__button--fix" onClick={() => navigate('/')}>Назад</button>
                 </div>
 
-                {/* Правая колонка */}
                 <div className="project__media">
                     <div className="project__video">
                         <video autoPlay muted controls loop>
@@ -51,21 +45,13 @@ export default function Project() {
                             Ваш браузер не поддерживает видео.
                         </video>
                     </div>
-                    <div className="project__buttons-container">
-                        <button 
-                            className={`project__button ${canClickPrev ? 'project__button--active' : 'project__button--deactive'}`} 
-                            onClick={prevProject}
-                            disabled={!canClickPrev}
-                        >
-                            Предыдущий проект
-                        </button>
-                        <button 
-                            className={`project__button ${canClickNext ? 'project__button--active' : 'project__button--deactive'}`} 
-                            onClick={nextProject}
-                            disabled={!canClickNext}
-                        >
-                            Следующий проект
-                        </button>
+                </div>
+
+                <div className="project__buttons-container">
+                    <button className="project__button project__button--fix" onClick={() => navigate('/')}>Назад</button>
+                    <div className="project__nav-buttons">
+                        <button className={`project__button ${canClickPrev ? 'project__button--active' : 'project__button--deactive'}`} onClick={prevProject} disabled={!canClickPrev}>Предыдущий проект</button>
+                        <button className={`project__button ${canClickNext ? 'project__button--active' : 'project__button--deactive'}`} onClick={nextProject} disabled={!canClickNext}>Следующий проект</button>
                     </div>
                 </div>
             </div>
